b4538768fdcd9fbca9d56e2786f8477bb68ccc0a
Vamsi Manne <vmanne20@Vamsis-MacBook-Pro.local>
2019-03-18 19:27:46 -0400

    create 3 endpoints for Blabber app using Express.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const blabs = [];
+uidCount = 0;
+
+app.get('/blabs/:createdSince', (req, res) => {
+    
+    let output = [];
+    for (let i = 0; i < blabs.length; i++) {
+        if (blabs[i].postTime >= req.params.createdSince)
+            output.push(blabs[i]);
+    }
+    res.send(output);
+});
+
+app.post('/blabs', (req, res) => {
+    
+    const newBlab = {
+        id : JSON.stringify(uidCount),
+        postTime: Date.getTime(),
+        author: req.body.author,
+        message: req.body.message,
+    }
+    blabs.push(newBlab);
+    uidCount++;
+    res.status(201).send(`Blab created successfully.`);
+});
+
+app.delete('/blabs/:id', (req, res) => {
+
+    for (let i = 0; i < blabs.length; i++) {
+        if (blabs[i].id === req.params.id) {
+            blabs.splice(i, 1);
+            res.status(200).send(`Blab deleted successfully.`);
+            return;
+        }
+    }
+    res.status(404).send(`Blab not found`);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
